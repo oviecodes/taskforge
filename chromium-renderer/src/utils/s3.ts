1a61b66c75d212e06a1ab8d63535a8ba5bc0fb61
Alexander Godwin <alecgee73@gmail.com>
2025-09-01 07:11:12 +0100

    s3 failure metrics

--- chromium-renderer/src/utils/s3.ts
+++ chromium-renderer/src/utils/s3.ts
@@ -7,8 +7,12 @@ import {
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 import { v4 as uuidv4 } from "uuid"
 import dotenv from "dotenv"
+import { withLogContext } from "./log-context"
+import { s3UploadFailures } from "./metrics"
 
 dotenv.config()
+
+const log = withLogContext({ service: "chromium-renderer" })
 
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
@@ -24,26 +28,36 @@ export async function uploadBufferToS3(
   keyPrefix = "pdf",
   mimeType = "application/pdf"
 ): Promise<string> {
-  const filename = `${keyPrefix}/${task_id}.pdf`
-  const bucket = process.env.S3_BUCKET_NAME as string
+  try {
+    const filename = `${keyPrefix}/${task_id}.pdf`
+    const bucket = process.env.S3_BUCKET_NAME as string
 
-  const uploadParams: PutObjectCommandInput = {
-    Bucket: bucket,
-    Key: filename,
-    Body: buffer,
-    ContentType: mimeType,
+    const uploadParams: PutObjectCommandInput = {
+      Bucket: bucket,
+      Key: filename,
+      Body: buffer,
+      ContentType: mimeType,
+    }
+
+    await s3.send(new PutObjectCommand(uploadParams))
+
+    // Create a signed URL that expires in 10 minutes
+    const signedUrl = await getSignedUrl(
+      s3,
+      new GetObjectCommand({ Bucket: bucket, Key: filename }),
+      { expiresIn: 600 } // 600 seconds = 10 mins
+    )
+
+    return signedUrl
+  } catch (e) {
+    log.error(
+      {
+        e,
+      },
+      "S3 upload error"
+    )
+    s3UploadFailures.labels({ type: "generate-pdf" }).inc()
+
+    throw new Error("S3 upload error")
   }
-
-  await s3.send(new PutObjectCommand(uploadParams))
-
-  // Create a signed URL that expires in 10 minutes
-  const signedUrl = await getSignedUrl(
-    s3,
-    new GetObjectCommand({ Bucket: bucket, Key: filename }),
-    { expiresIn: 600 } // 600 seconds = 10 mins
-  )
-
-  console.log(signedUrl)
-
-  return signedUrl
 }
